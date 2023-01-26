844b3ac9cff724e59f272d2582a9829d13636eb1
kraine <kraine212@gmail.com>
2023-01-26 17:02:37 +0200

    Methods part 1

--- Source/src/js/main.js
+++ Source/src/js/main.js
@@ -2,11 +2,13 @@ import './lib/lib';
 import $ from './lib/lib';
 
 $('button').on('click', function() {
-    $(this).toggleClass('active');
-    console.log(this);
+    $('div').eq(2).toggleClass('active');
 });
 
+$('div').click(function() {
+    console.log($(this).index())
+});
 
-function sayHello () {
-    console.log('hello');
-}
+console.log($('div').eq(2).find('.some'));
+
+    
