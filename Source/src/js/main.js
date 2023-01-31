63ba5f1de28402d0668c1a353d9afd68b246f47c
kraine <kraine212@gmail.com>
2023-01-31 16:58:40 +0200

    Styles lib
    Made style components

--- Source/src/js/main.js
+++ Source/src/js/main.js
@@ -1,15 +1,3 @@
 import './lib/lib';
 import $ from './lib/lib';
 
-$('button').on('click', function() {
-    $('div').eq(2).toggleClass('active');
-});
-
-$('div').click(function() {
-    console.log($(this).index())
-});
-
-// console.log($('div').eq(2).find('.some'));
-// console.log($('.more').siblings());
-$('.findme').fadeIn(1800);
-    
