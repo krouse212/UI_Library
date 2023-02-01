0371e291cd600344ce16d6bbc69e75ce1e17cfb1
kraine <kraine212@gmail.com>
2023-02-01 08:54:19 +0200

    FadeToggle
    add method in lib

--- Source/src/js/main.js
+++ Source/src/js/main.js
@@ -1,3 +1,14 @@
 import './lib/lib';
 import $ from './lib/lib';
 
+$('.btn').eq(0).on('click', () => {
+    $('.w-500').eq(0).fadeToggle(800);
+});
+
+$('.btn').eq(1).on('click', () => {
+    $('.w-500').eq(1).fadeToggle(800);
+});
+
+$('.btn').eq(2).on('click', () => {
+    $('.w-500').fadeToggle(800);
+});
